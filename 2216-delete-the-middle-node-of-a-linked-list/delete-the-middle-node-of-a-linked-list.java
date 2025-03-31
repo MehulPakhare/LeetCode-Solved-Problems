09df361bb43c638fa9b0b8bca4e569dc8b1535ca
Mehul Pakhare <70009482+MehulPakhare@users.noreply.github.com>
2025-03-31 19:37:44 +0530

    Time: 3 ms (99.79%) | Memory: 64.8 MB (24.81%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -8,44 +8,44 @@
 //  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 //  * }
 //  */
-// class Solution {
-//     public ListNode deleteMiddle(ListNode head) {
-        
-//         if(head.next == null) return null;
-
-//         ListNode slow = head;
-//         ListNode fast = head.next.next;
-
-        
-
-//         while(fast != null && fast.next != null){
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-
-//         slow.next = slow.next.next;
-
-//         return head;
-//     }
-// }
-
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        // Edge case: return nullptr if there is only one node.
-        if (head.next == null)
-            return null;
         
-        // Initialize two pointers, 'slow' and 'fast'.
-        ListNode slow = head, fast = head.next.next;
+        if(head.next == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+
         
-        // Let 'fast' move forward by 2 nodes, 'slow' move forward by 1 node each step.
-        while (fast != null && fast.next != null) {
+
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        
-        // When 'fast' reaches the end, remove the next node of 'slow' and return 'head'.
+
         slow.next = slow.next.next;
+
         return head;
     }
 }
+
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         // Edge case: return nullptr if there is only one node.
+//         if (head.next == null)
+//             return null;
+        
+//         // Initialize two pointers, 'slow' and 'fast'.
+//         ListNode slow = head, fast = head.next.next;
+        
+//         // Let 'fast' move forward by 2 nodes, 'slow' move forward by 1 node each step.
+//         while (fast != null && fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+        
+//         // When 'fast' reaches the end, remove the next node of 'slow' and return 'head'.
+//         slow.next = slow.next.next;
+//         return head;
+//     }
+// }
